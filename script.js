19a96a93b46e98423b0ddcfa57c4ddf55b014be5
Romka2005 <svetanok12@gmail.com>
2022-02-18 00:26:47 +0200

    резюме

---
+++ script.js
@@ -0,0 +1,48 @@
+
+//1
+
+// function display_message() {
+//     alert("JS is AWESOME");
+// }
+// display_message();
+
+
+//2
+
+// function favorite_book(title){
+//     alert(`One of my favorite books is ${title} in Wonderful`);
+// }
+// let t = prompt('Enter the title of your favorite book');
+// favorite_book(t);
+
+
+//3
+
+// function make_shirt(size, text){
+//     alert(`Shirt has a size - ${size} \nWritten on a shirt - ${text}`);
+// }
+// let size = prompt("Enter shirt's size");
+// let title = prompt("Enter shirt's title");
+// make_shirt(size,title);
+
+
+//4
+
+// function make_shirt(size, text){
+//     alert(`Shirt has a size - ${size} \nWritten on a shirt - ${text}`);
+// }
+// let s = prompt("Enter shirt's size");
+// let t = prompt("Enter shirt's title");
+// make_shirt("L", "I love Python");
+// make_shirt(s, t);
+
+
+//5
+
+// function describe_city(city, country) {
+//     alert(`${city} is in ${country}`);
+// }
+
+// city = prompt('Enter the name of any city');
+// country = prompt('Enter the name of any country');
+// describe_city(city, country);
